8dce378bc997a2001bfa8f9bd70923cba937ac7a
tamarru475 <tamarrubin@Tamars-MacBook-Air.local>
2022-10-19 15:48:56 +0300

    changing ports and Url back to deploy

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ require('dotenv').config();
 var cors = require('cors');
 
 const { MONGODB_URI = 'mongodb://localhost:27017/aroundb' } = process.env;
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 mongoose.connect(MONGODB_URI);
