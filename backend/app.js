2164013d6f5adc76eeaef05721a03745819d97f5
tamarru475 <tamarrubin@Tamars-MacBook-Air.local>
2022-10-21 12:22:10 +0300

    path to env

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,13 @@ app.use(express.json());
 app.use(cors());
 app.options('*', cors());
 
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', '*');
+  res.append('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+  res.append('Access-Control-Allow-Headers', 'Content-Type, authorization');
+  next();
+});
+
 app.use(requestLogger);
 
 app.get('/crash-test', () => {
