691a39839cad8b8a81c735a3f033227596a39a68
tamarru475 <tamarrubin@Tamars-MacBook-Air.local>
2022-10-21 11:35:45 +0300

    path to env

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
-require('dotenv').config( '../.env' );
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = { NODE_ENV, JWT_SECRET };
